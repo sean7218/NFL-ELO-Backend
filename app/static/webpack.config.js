68e7c249877d1d22d19d6000147e29a1905faf9c
Sean Zhang <sean7218@gmail.com>
2017-10-10 20:25:36 -0500

    adding reactjs

--- app/static/webpack.config.js
+++ app/static/webpack.config.js
@@ -8,5 +8,25 @@ const config = {
     resolve: {
         extensions: ['.js', '.jsx', '.css']
     },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader',
+                })
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: 'file-loader'
+            }
+        ]
+    },
 };
 module.exports = config;
