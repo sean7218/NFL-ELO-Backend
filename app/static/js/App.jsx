a1e9dcc1b329ef5afec2c51a13b52ef84d48f4a7
Sean Zhang <sean7218@gmail.com>
2017-10-11 22:52:04 -0500

    reactjs work2

--- app/static/js/App.jsx
+++ app/static/js/App.jsx
@@ -1,28 +1,70 @@
 import React from "react";
 
-const TodoForm = function({addTodo}) {
-  let input;
+// First Method Creating a Component
+// class AppS extends React.Component {
+//   render() {
+//     return <h1>Hello World</h1>
+//   }
+// }
 
-  return (
-    <div>
-      <input ref={} />
-      <button></button>
-    </div>
-  );
-}
-const Todo = ({todo, remove}) => {
-  return (<li onClick(remove(todo.id))> {todo.text}</li>)
-}
+// Second Method Creating a Component
+// const AppS = () => <h1>Hello Stateless</h1>
 
-const Title = () => {
-  return (
-    <div>
-      <div>
-        <h1>to-do</h1>
-      </div>
-    </div>
-  );
-}
+// Third Point: Making You Wrap All in One Div
+// class AppS extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello World</h1>
+//         <b>Bold</b>
+//       </div>
+//     )
+//   }
+// }
+
+// Fourth Point: Passing data to Component and default values
+// class AppS extends React.Component {
+//   render(){
+//     let txt = this.props.txt
+//     return <h1>{txt}</h1>
+//   }
+// }
+// AppS.propTypes = {
+//   txt: React.PropTypes.string,
+//   cat: React.PropTypes.number.isRequired
+// }
+// AppS.defualtProps = {
+//   txt: "this is the "
+// }
+
+// // Fifth Point: manage React Component State with setState
+// class AppS extends React.Component {
+
+//   constructor() {
+//     super();
+//     this.state = {
+//       txt: "this is the state txt"
+//     }
+//   }
+//   // This is custome component
+//   update(e) {
+//     this.setState({txt: e.target.value})
+//   }
+
+//   render(){
+//     return (
+//       <div>
+//         <input type="text" onChange={this.update.bind(this)} />
+//         <h1>{this.state.txt}</h1>
+        
+//       </div>
+//     )
+//   }
+// }
+
+const Widget = (props) =>  <input type="text" onChange={props.update} />
+  
+
 export default class App extends React.Component {
   render () {
     return <p> Hello React!</p>;
